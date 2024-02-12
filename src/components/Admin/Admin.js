c7b473c4ff228565e1aeeddabb6bd3a290c2ce03
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-02-12 09:33:55 +0100

    Add adminService.js and update Admin.js with adminService usage

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,11 +1,34 @@
 import { IconButton, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Box, Grid, Paper, Stack } from "@mui/material";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import AdminService from "../../services/adminService";
 
 function AdminPage() {
   const { t } = useTranslation();
+  const [admin, setAdmin] = useState("");
+  const [serialNumber, setSerialNumber] = useState("");
+
+  useEffect(() => {
+    getAdmin();
+  }, []);
+
+  async function getAdmin() {
+    const result = await AdminService.getAdmin();
+    console.log("result", result);
+    setAdmin(result);
+    console.log(result[0]);
+    setSerialNumber(result.serialnumber);
+  }
+
+  async function updateSerialNumber(event) {
+    setSerialNumber(event.target.value);
+    console.log("test");
+    const adminUpdates = { ...admin, serialNumber };
+    await AdminService.updateAdmin(adminUpdates);
+  }
+
 
   return (
     <Grid item>
@@ -44,6 +67,8 @@ function AdminPage() {
                 fullWidth
                 id="standard-basic"
                 autoComplete="off"
+                value={serialNumber}
+                onChange={updateSerialNumber}
               />
             </Stack>
           </Box>
